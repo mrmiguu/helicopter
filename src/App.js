de258a4776ee9efcb93fb326db62d7c0c612141c
Alex <amatulionis@virtual-corp.com>
2019-01-04 11:18:38 -0800

    Initial commit.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+
+import './styles/App.css'
+
+import logo from './assets/logo.png'
 
 class App extends Component {
   render() {
@@ -21,8 +23,8 @@ class App extends Component {
           </a>
         </header>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
